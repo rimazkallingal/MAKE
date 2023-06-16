c05994c2bbf5d73e4c4f854a15530d35b03f556e
rimazkallingal <125657211+rimazkallingal@users.noreply.github.com>
2023-06-16 17:57:43 +0530

    Add files via upload

---
+++ js/mobile-menu.js
@@ -0,0 +1,14 @@
+function openmenu() {
+  $(".navbar").addClass("active");
+}
+function closemenu() {
+  $(".navbar").removeClass("active");
+}
+
+// $(".fa-bars").click(function () {
+//     $(".navbar").addClass("active");
+//   });
+
+//   $(".fa-times").click(function () {
+//     $(".navbar").removeClass("active");
+//   });
